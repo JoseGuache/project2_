929969a518968d648f93d7ce2438dfad981b8c21
James Guache <jamesg2126@gmail.com>
2024-11-14 19:33:47 -0500

    save for mac

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,12 @@
 // Seperated for login out of best practice.
+
+const withAuth = (req, res, next) => {
+    // If the user isn't logged in, redirect them to the login route.
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
