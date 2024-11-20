bb63c5b93dcc09ba2fc07c92612585035303ce00
John Ferlito <johnferlito73@gmail.com>
2024-11-19 22:24:40 -0500

    add footer

--- utils/helpers.js
+++ utils/helpers.js
@@ -16,5 +16,8 @@ module.exports = {
   },
   format_amount: (amount) => {
     return parseInt(amount).toLocaleString();
+  },
+  getCurrentYear: () => {
+    return new Date().getFullYear();
   }
 };
